ae9fa58fb60341e81faaaa23e0e1a0d73d526f41
yash.patel <yash@prakashinfotech.com>
2022-05-20 17:19:56 +0530

    Up to UserAdapter.kt done

---
+++ app/src/main/java/com/yash/recyclervieweditdelete/adapter/UserAdapter.kt
@@ -0,0 +1,112 @@
+package com.yash.recyclervieweditdelete.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.RecyclerView
+import com.yash.recyclervieweditdelete.R
+
+class UserAdapter(val context: Context,val userList: ArrayList<com.yash.recyclervieweditdelete.model.UserData>):RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+
+    inner class UserViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+        var name:TextView
+        var mbNum:TextView
+        var mMenus: ImageView
+
+        init {
+            name = itemView.findViewById(R.id.mTitle)
+            mbNum = itemView.findViewById(R.id.mSubTitle)
+            mMenus = itemView.findViewById(R.id.mMenus)
+            mMenus.setOnClickListener { popupMenus(it) }
+        }
+
+        private fun popupMenus(view: View?) {
+
+            val position = userList[adapterPosition]
+            val popupMenus = PopupMenu(context,view)
+            popupMenus.inflate(R.menu.show_menu)
+            popupMenus.setOnMenuItemClickListener {
+                when(it.itemId){
+                    R.id.editText ->{
+                        val v = LayoutInflater.from(context).inflate(R.layout.add_item,null)
+                        val name = v.findViewById<EditText>(R.id.userName)
+                        val number = v.findViewById<EditText>(R.id.userNo)
+                        AlertDialog.Builder(context)
+                            .setView(v)
+                            .setPositiveButton("ok"){
+                                dialog,_ ->
+                                position.userName = name.text.toString()
+                                position.userMb = number.text.toString()
+                                notifyDataSetChanged()
+                                Toast.makeText(context,"User Information is Edited",Toast.LENGTH_SHORT).show()
+                                dialog.dismiss()
+                            }
+                            .setNegativeButton("Cancel"){
+                                dialog,_ ->
+                                dialog.dismiss()
+                            }
+                            .create()
+                            .show()
+
+                        true
+                    }
+
+                    R.id.delete -> {
+
+                        /**set delete*/
+                        android.app.AlertDialog.Builder(context)
+                            .setTitle("Delete")
+                            .setIcon(R.drawable.ic_warning)
+                            .setMessage("Are you sure delete this Information")
+                            .setPositiveButton("Yes"){
+                                    dialog,_->
+                                userList.removeAt(adapterPosition)
+                                notifyDataSetChanged()
+                                Toast.makeText(context,"Deleted this Information",Toast.LENGTH_SHORT).show()
+                                dialog.dismiss()
+                            }
+                            .setNegativeButton("No"){
+                                    dialog,_->
+                                dialog.dismiss()
+                            }
+                            .create()
+                            .show()
+                        true
+                    }
+                    else -> true
+
+                }
+
+            }
+            popupMenus.show()
+            val popup = PopupMenu::class.java.getDeclaredField("mPopup")
+            popup.isAccessible = true
+            val menu = popup.get(popupMenus)
+            menu.javaClass.getDeclaredMethod("setForceShowIcon",Boolean::class.java)
+                .invoke(menu,true)
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.list_item,parent,false)
+        return UserViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val newList = userList[position]
+        holder.name.text = newList.userName
+        holder.mbNum.text = newList.userMb
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+}
---
+++ app/src/main/java/com/yash/recyclervieweditdelete/model/UserData.kt
@@ -0,0 +1,3 @@
+package com.yash.recyclervieweditdelete.model
+
+data class UserData(var userName : String,var userMb: String)
